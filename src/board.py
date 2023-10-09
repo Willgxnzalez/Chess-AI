e83ab65a705e2e2df18ee5324666ef3084e9a4c5
Wilberth Gonzalez <wilbertg@uci.edu>
2023-10-09 11:26:34 -0700

    Each piece will calculate their moveset

--- src/board.py
+++ src/board.py
@@ -6,6 +6,13 @@ class Board(list):
     def __init__(self) -> None:
         super().__init__([Square(r,c) for c in range(COLS)] for r in range(ROWS))
 
+    @staticmethod
+    def in_range(*indices):
+        for i in indices:
+            if i < 0 or i > 7:
+                return False
+        return True
+    
     def populate_board(self, color) -> None:
         others, pawns = (0, 1) if color == "black" else (7, 6)
         
@@ -21,9 +28,7 @@ class Board(list):
         self[others][6].set_piece(Knight(color))
         self[others][7].set_piece(Rook(color))
 
-    @staticmethod
-    def in_range(*indices):
-        for i in indices:
-            if i < 0 or i > 7:
-                return False
-        return True
+    def calc_valid_moves(self):
+        pass
+
+
--- src/piece.py
+++ src/piece.py
@@ -1,4 +1,3 @@
-from typing import Any
 from const import *
 import pygame
 import os
@@ -19,7 +18,7 @@ class Pawn(Piece):
         self.direction = -1 if color == "white" else 1
         super().__init__("pawn", color, 1.0)
 
-    def calc_valid_moves(self):
+    def get_candidate_moves(self, row, col):
         pass
 
 class Rook(Piece):
@@ -27,22 +26,44 @@ class Rook(Piece):
         self.direction = -1 if color == "white" else 1
         super().__init__("rook", color, 5.0)
 
+    def get_candidate_moves(self, row, col):
+        pass
+
 class Knight(Piece):
     def __init__(self, color: str) -> None:
         self.direction = -1 if color == "white" else 1
         super().__init__("knight", color, 3.0)
+
+    def get_candidate_moves(self, row, col):
+       return ((row-2, col+1),
+                (row-1, col+2),
+                (row+1, col+2),
+                (row+2, col+1),
+                (row+2, col-1),
+                (row+1, col-2),
+                (row-1, col-2),
+                (row-2, col-2))
 
 class Bishop(Piece):
     def __init__(self, color: str) -> None:
         self.direction = -1 if color == "white" else 1
         super().__init__("bishop", color, 3.1)
 
+    def get_candidate_moves(self, row, col):
+        pass
+
 class Queen(Piece):
     def __init__(self, color: str) -> None:
         self.direction = -1 if color == "white" else 1
         super().__init__("queen", color, 9.0)
 
+    def get_candidate_moves(self, row, col):
+        pass
+
 class King(Piece):
     def __init__(self, color: str) -> None:
         self.direction = -1 if color == "white" else 1
         super().__init__("king", color, 5000.0)
+
+    def get_candidate_moves(self, row, col):
+        pass
