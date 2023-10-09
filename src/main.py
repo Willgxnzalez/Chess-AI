d628698b34a81994b811df0aa40a0369c77a6af3
Wilberth Gonzalez <wilbertg@uci.edu>
2023-10-08 18:51:15 -0700

    initial commit

---
+++ src/main.py
@@ -0,0 +1,13 @@
+from const import *
+import pygame
+
+class App:
+    def __init__(self):
+        pass
+
+    def mainloop(self):
+        pass
+
+if __name__ == "__main__":
+    app = App()
+    app.mainloop()
