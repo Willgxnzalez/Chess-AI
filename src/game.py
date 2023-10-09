331bd9ee013017a9f00d0e957ec2fd9bac95aa31
Wilberth Gonzalez <wilbertg@uci.edu>
2023-10-08 19:12:26 -0700

    implement mainloop in App class

---
+++ src/game.py
@@ -0,0 +1,8 @@
+import pygame
+
+class Game:
+    def __init__(self):
+        pass
+
+    def render_bg(self, surface):
+        pass
--- src/main.py
+++ src/main.py
@@ -1,12 +1,28 @@
 from const import *
+import sys
 import pygame
+from game import Game
 
 class App:
     def __init__(self):
-        pass
+        self.win = pygame.display.set_mode((WIDTH, HEIGHT))
+        self.engine = Game()
 
     def mainloop(self):
-        pass
+        run = True
+        while run:
+
+
+            for E in pygame.event.get():
+                if E.type == pygame.QUIT:
+                    run = False
+
+
+            pygame.display.update()
+
+        pygame.quit()
+        sys.exit()
+
 
 if __name__ == "__main__":
     app = App()
